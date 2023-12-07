1ab055da87666549f2e6aa265ceea8f6465d3f49
call me ai|sem <aimur349Gmail.com>
2023-12-07 20:58:23 +0600

    1st commit

---
+++ MyClass.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static PracticeWork.Program;
+
+namespace PracticeWork
+{
+    public class MyClass : IPropertyChanged
+    {
+        private string _name;
+
+        public event PropertyEventHandler PropertyChanged;
+
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if (_name != value)
+                {
+                    _name = value;
+                    OnPropertyChanged(nameof(Name));
+                }
+            }
+        }
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyEventArgs(propertyName));
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,10 +6,43 @@ using System.Threading.Tasks;
 
 namespace PracticeWork
 {
+    /// <summary>
+    /// Создать интерфейс, который уведомляет пользователя программы об изменении значений свойства в классе 
+    /// (в котором реализуется данный интерфейс). 
+    /// Сигнатура интерфейса:
+    /// </summary>
+
+    public delegate void PropertyEventHandler(object sender, PropertyEventArgs e);
+
+    public class PropertyEventArgs : EventArgs
+    {
+        public string PropertyName { get; }
+
+        public PropertyEventArgs(string propertyName)
+        {
+            PropertyName = propertyName;
+        }
+    }
+
+    public interface IPropertyChanged
+    {
+        event PropertyEventHandler PropertyChanged;
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
+                     MyClass myObject = new MyClass();
+
+            myObject.PropertyChanged += HandlePropertyChanged;
+
+                        myObject.Name = "Новое значение";
+        }
+
+
+        static void HandlePropertyChanged(object sender, PropertyEventArgs e)
+        {
+            Console.WriteLine($"Свойство {e.PropertyName} было изменено.");
         }
     }
 }
